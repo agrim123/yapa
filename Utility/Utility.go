5a6e989df68489a278b9112c63fe237b8e6c1089
Agrim Mittal <agrimmittal97@gmail.com>
2018-04-29 03:22:10 +0530

    Update help

--- Utility/Utility.go
+++ Utility/Utility.go
@@ -15,26 +15,28 @@ func Help(endstring string) {
    yapa - Yet Another Personal Assistant
 	`
 	const usage = `USAGE:
-   yapa [command]
+   yapa [global options] command [command options] [arguments...]
 	`
 	version := fmt.Sprintf(`VERSION:
    %s
    `, VERSION)
 
 	const commands = `COMMANDS:
-   help, h                   Display help
+   setup                     Setup a new yapa profile
+   clean                     Clear all yapa settings
    count                     Count number of files/folders in directory
-   key                       Print current user public key
-   ping                      Check if online
+   key                       Get current user's public key
+   ping [HOSTNAME]           Check if host is online
    list                      List all servers listed in config.json
    bye                       Shutdown system
-   uptime                    Display uptime of a server
+   uptime [USER] [IP]        Display uptime of a server
    cool
    hackernews, hn            Display Hacker News
    scan                      Scan a hostname
    toss                      Flips a coin
-   dice                      Roll a dice`
-	const flags = `FLAGS:
+   dice                      Roll a dice
+   help, h                   Display help`
+	const flags = `GLOBAL OPTIONS:
    -h, --help     Display help
 	`
 
